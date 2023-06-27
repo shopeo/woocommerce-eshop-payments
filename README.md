7aa06e6f36b9c5ce0357af159edac0e229da4e7b
jabber <2898117012@qq.com>
2023-06-27 15:08:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# WooCommerce eshop Payments
---
+++ woocommerce-eshop-payments.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Eshop Payments
+ * Plugin URI:  https://woocommerce.com/products/woocommerce-eshop-payments/
+ * Description: eshop complete payments processing solution.
+ * Version:     1.9.2
+ * Author:      SHOPEO
+ * Author URI:  https://www.shopeo.cn/
+ * License:     GPL-2.0
+ * Requires PHP: 7.1
+ * WC requires at least: 3.9
+ * WC tested up to: 6.7
+ * Text Domain: woocommerce-eshop-payments
+ *
+ */
+require_once 'vendor/autoload.php';
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
