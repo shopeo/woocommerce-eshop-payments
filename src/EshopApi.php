d811a9c5068cbb7cc6255fe447a20cad22e7728d
jabber <2898117012@qq.com>
2023-06-29 02:57:47 +0800

    init

---
+++ src/EshopApi.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Shopeo\WoocommerceEshopPayments;
+
+class EshopApi {
+	public $point = 'http://api.store.eshop-pay.com';
+	public $app_id = '';
+	public $app_secret = '';
+
+	public function __construct( $app_id, $app_secret ) {
+		$this->app_id     = $app_id;
+		$this->app_secret = $app_secret;
+	}
+
+	private static function getMillisecond() {
+		$time  = explode( " ", microtime() );
+		$time1 = explode( ".", $time[0] * 1000 );//  $time[0] * 1000;
+		if ( $time1[0] < 10 ) {
+			$time2 = $time[1] . "00" . $time1[0];
+		} else if ( $time1[0] < 100 ) {
+			$time2 = $time[1] . "0" . $time1[0];
+
+		} else {
+			$time2 = $time[1] . $time1[0];
+		}
+
+		return $time2;
+	}
+
+	public function signature( $timestamp, $nonce_str = 'wc_gateway_eshop' ) {
+		$originString = $this->app_id . '&' . $timestamp . '&' . $nonce_str . '&' . $this->app_secret;
+
+		return strtoupper( md5( $originString ) );
+	}
+
+	public function makeOrder( $data ) {
+		$url       = $this->point . '/api/omipay/order/create';
+		$timestamp = $this::getMillisecond();
+		$nonce_str = 'wc_gateway_eshop';
+		$sign      = $this->signature( $timestamp, $nonce_str );
+		$body      = json_encode( $data );
+		error_log( $body );
+		$response = wp_remote_post( $url, array(
+			'headers' => array(
+				'appId'     => $this->app_id,
+				'nonceStr'  => $nonce_str,
+				'sign'      => $sign,
+				'timestamp' => $timestamp
+			),
+			'body'    => $body
+		) );
+		//返回结果
+		if ( ! is_wp_error( $response ) ) {
+			return array(
+				'body' => $response['body']
+			);
+		} else {
+			wc_add_notice( __( 'Please use direct bank deposit.', 'woocommerce-eshop-payments' ), 'error' );
+
+			return;
+		}
+	}
+}
--- woocommerce-eshop-payments.php
+++ woocommerce-eshop-payments.php
@@ -13,6 +13,9 @@
  * Text Domain: woocommerce-eshop-payments
  *
  */
+
+use Shopeo\WoocommerceEshopPayments\EshopApi;
+
 require_once 'vendor/autoload.php';
 
 
@@ -64,7 +67,110 @@ if ( ! function_exists( 'woocommerce_gateway_eshop_init' ) ) {
 		}
 		if ( ! class_exists( 'WC_Eshop_Gateway' ) ) {
 			class WC_Eshop_Gateway extends WC_Payment_Gateway {
-				
+				public $app_id = '';
+				public $app_secret = '';
+
+				public function __construct() {
+					$this->id                 = 'eshop_pay';
+					$this->icon               = plugins_url( '/assets/images/paymethod.jpg', WOOCOMMERCE_ESHOP_PAYMENTS_PLUGIN_FILE );
+					$this->method_title       = __( 'Debit / Credit Card Payment', 'woocommerce-eshop-payments' );
+					$this->method_description = __( 'Please note the payment times will be take a bit time please don’t refresh the page Your order will not be shipped until the payment been successfully, if the payment fails, please use bank transfer to pay, or contact us directly through online consultation, any issues and questions please contact us.', 'woocommerce-eshop-payments' );
+
+					$this->title       = $this->method_title;
+					$this->description = $this->method_description;
+
+					$this->supports = array(
+						'products'
+					);
+					$this->init_form_fields();
+
+					$this->init_settings();
+					$this->enabled    = $this->get_option( 'enabled' );
+					$this->app_id     = $this->get_option( 'app_id' );
+					$this->app_secret = $this->get_option( 'app_secret' );
+
+					add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array(
+						$this,
+						'process_admin_options'
+					) );
+					add_action( 'woocommerce_api_' . $this->id, array( $this, 'webhook' ) );
+				}
+
+				public function init_form_fields() {
+					$this->form_fields = array(
+						'enabled'    => array(
+							'title'       => __( 'Enabled/Disable', 'woocommerce-eshop-payments' ),
+							'label'       => __( 'Enable OmiPay Gateway', 'woocommerce-eshop-payments' ),
+							'type'        => 'checkbox',
+							'description' => '',
+							'default'     => 'on'
+						),
+						'app_id'     => array(
+							'title'       => __( 'APPID', 'woocommerce-eshop-payments' ),
+							'type'        => 'text',
+							'description' => '',
+							'default'     => '',
+							'desc_tip'    => true,
+						),
+						'app_secret' => array(
+							'title'       => __( 'APP Secret', 'woocommerce-eshop-payments' ),
+							'type'        => 'text',
+							'description' => '',
+							'default'     => '',
+							'desc_tip'    => true,
+						)
+					);
+				}
+
+				public function process_payment( $order_id ) {
+					global $woocommerce;
+					$order    = new WC_Order( $order_id );
+					$data     = array(
+						'amount'               => $order->get_total() * 100,
+						'city'                 => $order->get_shipping_city(),
+						'countryTwoCode'       => $order->get_shipping_country(),
+						'currency'             => $order->get_currency(),
+						'language'             => 'EN',
+						'notifyUrl'            => home_url() . '/wc-api/' . $this->id . '?id=' . $order->get_id(),
+						'outOrderNo'           => $order->get_order_number(),
+						'payType'              => 'omipay',
+						'postCode'             => $order->get_shipping_postcode(),
+						'province'             => $order->get_shipping_state(),
+						'recipientContactInfo' => $order->get_billing_phone() ?: $order->get_billing_email(),
+						'recipientName'        => $order->get_shipping_first_name() . ' ' . $order->get_shipping_last_name(),
+						'redirectUrl'          => $this->get_return_url( $order ),
+						'streetAddress'        => $order->get_shipping_address_1() . ' ' . $order->get_shipping_address_2()
+					);
+					$eshopApi = new EshopApi( $this->app_id, $this->app_secret );
+					$response = $eshopApi->makeOrder( $data );
+					error_log( print_r( $response['body'], true ) );
+					$res = json_decode( $response['body'] );
+					if ( $res->code === 200 ) {
+						error_log( $res->data );
+					}
+				}
+
+				public function webhook() {
+					$order = new WC_Order( $_GET['id'] );
+					error_log( 'WebHook:' . $_GET['id'] );
+					$params = file_get_contents( "php://input" );
+					error_log( print_r( $params, true ) );
+					$response  = json_decode( $params );
+					$sign      = '';
+					$timestamp = '';
+					$nonce_str = '';
+					$eshopApi  = new EshopApi( $this->app_id, $this->app_secret );
+					$sign1     = $eshopApi->signature( $timestamp, $nonce_str );
+					if ( $sign == $sign1 ) {
+						error_log( 'Payment Complete' );
+						$order->payment_complete();
+						wc_reduce_stock_levels( $order->get_id() );
+						echo json_encode( array( 'return_code' => 'SUCCESS' ) );
+					} else {
+						error_log( 'Payment Fail' );
+						echo json_encode( array( 'return_code' => 'FAIL' ) );
+					}
+				}
 			}
 		}
 	}
